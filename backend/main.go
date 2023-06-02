f7cf7b6422c44c82056d3014226bba93671724bb
Baiqing Lyu <baiqinglyu@gmail.com>
2023-06-02 13:48:02 -0700

    Better handling of user prompts

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Starting app.")
+
+	// Start the RESTful server
+	go startServer()
+
+	// Run the systray
 	systray.Run(onReady, onExit)
+}
+
+func startServer() {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/userPrompt", handleUserPrompt).Methods("POST")
+
+	fmt.Println("Starting RESTful server on port 8080...")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
+}
+
+func handleUserPrompt(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		Prompt string `json:"prompt"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Process the user prompt here...
+	fmt.Println("Received user prompt:", requestData.Prompt)
+
+	// Send a response
+	response := struct {
+		Message string `json:"message"`
+	}{
+		Message: "Prompt received successfully",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // Set the HTTP status code to 200
+	json.NewEncoder(w).Encode(response)
 }
 
 func onExit() {
