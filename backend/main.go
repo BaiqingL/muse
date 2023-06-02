dc3473a33772a952f37d4933bfd772bea6626596
Baiqing Lyu <baiqinglyu@gmail.com>
2023-06-02 14:18:24 -0700

    revert

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 
-	"github.com/BaiqingL/conviction-commit-team-1"
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
 	"github.com/gorilla/mux"
@@ -42,7 +46,7 @@ func handleUserPrompt(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Query ChatGPT
-	response := query.QueryChatGPT(requestData.Prompt)
+	response := queryChatGPT(requestData.Prompt)
 	fmt.Println("ChatGPT response:", response)
 
 	// Send a response
@@ -55,6 +59,79 @@ func handleUserPrompt(w http.ResponseWriter, r *http.Request) {
 		Message:  "Prompt received successfully",
 		Response: response,
 	})
+}
+
+func queryChatGPT(prompt string) string {
+	type Message struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	}
+
+	type Request struct {
+		Model       string    `json:"model"`
+		Messages    []Message `json:"messages"`
+		Temperature float64   `json:"temperature"`
+	}
+
+	requestData := Request{
+		Model: "gpt-3.5-turbo",
+		Messages: []Message{
+			{
+				Role:    "user",
+				Content: prompt,
+			},
+		},
+		Temperature: 0.7,
+	}
+
+	requestBody, err := json.Marshal(requestData)
+	if err != nil {
+		fmt.Println("Error encoding request body:", err)
+		return ""
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return ""
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("OPENAI_API_KEY"))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return ""
+	}
+
+	var response struct {
+		Choices []struct {
+			Message struct {
+				Content string `json:"content"`
+			} `json:"message"`
+		} `json:"choices"`
+	}
+
+	err = json.Unmarshal(responseBody, &response)
+	if err != nil {
+		fmt.Println("Error parsing ChatGPT response:", err)
+		return ""
+	}
+
+	if len(response.Choices) > 0 {
+		return response.Choices[0].Message.Content
+	}
+
+	return ""
 }
 
 func onExit() {
