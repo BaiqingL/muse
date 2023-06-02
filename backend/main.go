827780df2de8745082569766ffe1d574a48b000b
Baiqing Lyu <baiqinglyu@gmail.com>
2023-06-02 14:06:39 -0700

    Add gpt layer

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
@@ -42,19 +45,93 @@ func handleUserPrompt(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Process the user prompt here...
-	fmt.Println("Received user prompt:", requestData.Prompt)
+	// Query ChatGPT
+	response := queryChatGPT(requestData.Prompt)
+	fmt.Println("ChatGPT response:", response)
 
 	// Send a response
-	response := struct {
-		Message string `json:"message"`
-	}{
-		Message: "Prompt received successfully",
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK) // Set the HTTP status code to 200
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(struct {
+		Message  string `json:"message"`
+		Response string `json:"response"`
+	}{
+		Message:  "Prompt received successfully",
+		Response: response,
+	})
+}
+
+func queryChatGPT(prompt string) string {
+	type Message struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	}
+
+	type Request struct {
+		Model       string    `json:"model"`
+		Messages    []Message `json:"messages"`
+		Temperature float64   `json:"temperature"`
+	}
+
+	requestData := Request{
+		Model: "gpt-3.5-turbo",
+		Messages: []Message{
+			{
+				Role:    "user",
+				Content: prompt,
+			},
+		},
+		Temperature: 0.7,
+	}
+
+	requestBody, err := json.Marshal(requestData)
+	if err != nil {
+		fmt.Println("Error encoding request body:", err)
+		return ""
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return ""
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("OPENAI_API_KEY"))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return ""
+	}
+
+	var response struct {
+		Choices []struct {
+			Message struct {
+				Content string `json:"content"`
+			} `json:"message"`
+		} `json:"choices"`
+	}
+
+	err = json.Unmarshal(responseBody, &response)
+	if err != nil {
+		fmt.Println("Error parsing ChatGPT response:", err)
+		return ""
+	}
+
+	if len(response.Choices) > 0 {
+		return response.Choices[0].Message.Content
+	}
+
+	return ""
 }
 
 func onExit() {
